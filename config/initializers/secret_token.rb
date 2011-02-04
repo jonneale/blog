c8ef7e0ea7102d82aaf7e4050dd54bf80f8168f1
Jon Neale <jon.neale@uswitch.com>
2011-02-04 17:07:29 +0000

    Span up rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '36d2b196af9dcfdedd7f7fb25b0c34fdff3494d36fada580f4b6181c36955f5db7a1ac060935d9cfb71f7a9d26ec86228674cde7c9554b78c29ea480af22d9b3'
